f5c31521563314d9ed892ce1c18ed4777a8e8cee
LucasRolon <lucasmarolonro050@gmail.com>
2024-08-29 21:16:55 -0300

    Add files via upload

---
+++ Paradigmas-uno.py
@@ -0,0 +1,12 @@
+temperatura=int
+
+temperatura = int((input("Ingrese la temperatura en Celsius: ")))
+
+if temperatura<0:
+ print ("Su temperatura corresponde a la categoría: Congelante.")
+if temperatura >= 0 and temperatura <= 15:
+    print ("Su temperatura es: Fría. ")
+if temperatura >= 16 and temperatura <= 25:
+    print ("Su temperatura es: Templada. ")
+if temperatura >= 25:
+    print ("Su temperatura es: Cálida. ")
