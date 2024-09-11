85d1f237d6bba864f1789f01c39397bc58d5c677
LucasRolon <lucasmarolonro050@gmail.com>
2024-09-10 21:12:38 -0300

    Update Paradigmas-uno.py

--- Paradigmas-uno.py
+++ Paradigmas-uno.py
@@ -1,6 +1,6 @@
-temperatura=int
+temperatura=float
 
-temperatura = int((input("Ingrese la temperatura en Celsius: ")))
+temperatura = float((input("Ingrese la temperatura en Celsius: ")))
 
 if temperatura<0:
  print ("Su temperatura corresponde a la categoría: Congelante.")
