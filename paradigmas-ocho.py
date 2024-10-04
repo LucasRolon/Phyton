c9f802b5d06d0f119a354003fd30042bc482f8d6
LucasRolon <lucasmarolonro050@gmail.com>
2024-10-03 23:48:34 -0300

    Create paradigmas-ocho.py

---
+++ paradigmas-ocho.py
@@ -0,0 +1,59 @@
+# Control de inventario de una tienda, con sus respectivos stocks, el programa deberá permitir transacciones como "Compra" o "Venta"
+# varias veces sin detenerse. Y al final mostrar las cantidades de esos productos.
+estado=int
+inventario = int
+ropavestir=int
+ropavestir=1
+pantalones=int
+pantalones=1
+camisetas=int
+camisetas=1
+vestidos=int
+vestidos=1
+
+while inventario!=0:
+    print("0. Salir. ")
+    print ("1. Ropa de vestir. ")
+    print("2. Pantalones. ")
+    print("3. Camisetas. ")
+    print("4. Vestidos. ")
+    inventario = int((input("Ingrese el nombre del producto: ")))
+    if inventario!=0:
+        print("1. Compra.")
+        print("2. Venta.")
+        estado = int((input("Ingrese su transaccion: ")))
+#------------ COMPRA ---------------
+    if estado==1 and inventario ==1:
+        ropavestir=ropavestir+1
+    if estado==1 and inventario ==2:
+        pantalones=pantalones+1
+    if estado==1 and inventario ==3:
+        camisetas=camisetas+1
+    if estado==1 and inventario == 4:
+        vestidos=vestidos+1
+#----------- VENTA -----------------
+    if estado==2 and inventario==1:
+        ropavestir=ropavestir-1
+        if ropavestir <= 0:
+            print("El producto deseado no se encuentra en stock. ")
+            ropavestir=0
+    if estado==2 and inventario == 2:
+        pantalones=pantalones-1
+        if pantalones <= 0:
+            print("El producto deseado no se encuentra en stock. ")
+            pantalones=0
+    if estado==2 and inventario ==3:
+        camisetas=camisetas-1
+        if camisetas <= 0:
+            print("El producto deseado no se encuentra en stock. ")
+            camisetas=0
+    if estado==2 and inventario == 4:
+        vestidos=vestidos-1
+        if vestidos <= 0:
+            print("El producto deseado no se encuentra en stock. ")
+            vestidos=0
+
+print("El total de ropa de vestir es de: ", ropavestir)
+print("El total de ropa de vestir es de: pantalones", pantalones)
+print("El total de ropa de vestir es de: camisetas", camisetas)
+print("El total de ropa de vestir es de: vestidos", vestidos)
